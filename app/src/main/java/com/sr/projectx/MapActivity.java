147c5a6c7499ed5b08d3cae24b7ea055ea5a9f04
SR <sdsddfs>
2016-11-22 21:38:42 +0200

    SR-22.11.16

--- app/src/main/java/com/sr/projectx/MapActivity.java
+++ app/src/main/java/com/sr/projectx/MapActivity.java
@@ -10,15 +10,34 @@ import android.view.ViewGroup;
 
 public class MapActivity extends Fragment {
 
-
+    private final LatLng HAMBURG = new LatLng(53.558, 9.927);
+    private final LatLng KIEL = new LatLng(53.551, 9.993);
+    private GoogleMap map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         View rootView = inflater.inflate(R.layout.map_fragment, container, false);
 
+        map = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        map.setMyLocationEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        map.getUiSettings().setMapToolbarEnabled(false);
+
+        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+                .title("Hamburg"));
+        Marker kiel = map.addMarker(new MarkerOptions()
+                .position(KIEL)
+                .title("Kiel")
+                .snippet("Kiel is cool")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.mipmap.ic_launcher)));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 
         return rootView;
     }
